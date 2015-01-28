0cc9cc20e11575c20e03c047eca8690c17442c76
Mitchell Mason <mmaso14@tigers.lsu.edu>
2015-01-27 22:21:21 -0600

    Created directories and some scene transistion code.

---
+++ DweebMcDermitt/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour {
+	
+	// Check for clicks and move on to the next scene
+	void Update () {
+		if(Input.anyKeyDown){
+			Application.LoadLevel("Intro");
+		}
+	}
+}
