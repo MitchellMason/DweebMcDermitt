6165f583c4648e9d729cdc99074b0229140f6305
Mitchell Mason <mmaso14@lsu.edu>
2015-02-05 11:27:26 -0600

    Started to add bear, and also fixed issue with the Models/ folder

---
+++ DweebMcDermitt/Assets/Scripts/Mirror.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mirror : LaserTarget {
+	public LaserTarget Monkey;
+
+	override public void onLaserShot(){
+		Monkey.onLaserShot ();
+	}
+}
---
+++ DweebMcDermitt/Assets/Scripts/Monkey.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monkey : LaserTarget {
+	//<summary>
+	//When hit by a laser, the color the object turns
+	//</summary>
+	public Color shotColor;
+	public AudioSource wonderful;
+
+	override public void onLaserShot(){
+		Color temp = gameObject.renderer.material.color;
+		this.gameObject.renderer.material.color = shotColor;
+		shotColor = temp;
+		
+		wonderful.Play ();
+	}
+}
