48f168519a1292082c031775b24fbbf9ef1de541
Mitchell Mason <mmaso14@tigers.lsu.edu>
2015-02-05 12:44:18 -0600

    Laser effect

---
+++ DweebMcDermitt/Assets/Scripts/LaserEffectControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserEffectControl : MonoBehaviour {
+	
+	public ParticleSystem laser;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Space)){
+			laser.Play();
+		}
+		else{
+			laser.Stop();
+			laser.SetParticles(null, 0);
+		}
+	}
+}
