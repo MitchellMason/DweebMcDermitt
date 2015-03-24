679f182db8ec18e43430a9cd067f151c67fc07ad
Cody Rogers <croge32@gmail.com>
2015-03-24 01:43:49 -0500

    grabbable class

--- DweebMcDermitt/Assets/Scripts/Interactable/Grabbable.cs
+++ DweebMcDermitt/Assets/Scripts/Interactable/Grabbable.cs
@@ -2,13 +2,43 @@ using UnityEngine;
 using System.Collections;
 
 public class Grabbable : InteractionTarget {
+
+	private GameObject player;
+	private Transform playerTransform;
+	private Vector3 playerPosition;
+
+	private Vector3 cameraPosition;
+	private GameObject hitObject;
+	private RaycastHit hit;
+	private Collider col;
+	private bool interactedOn = false;
 	
-	override public void onInteract() {
-		// float in front of the player or something idk
+	void Start(){
+		player = GameObject.Find ("Player");
+		playerTransform = player.transform;
+		col = GetComponent<Collider> ();
 	}
 	
-	//TODO
+	void Update () {
+		cameraPosition = Camera.main.transform.position;
+		playerPosition = playerTransform.position;
+		if (interactedOn) {
+			transform.parent = Camera.main.transform;
+
+			col.attachedRigidbody.isKinematic = true;
+		} else {
+			transform.parent = null;
+			//transform.position = new Vector3(transform.position.x, 1, transform.position.z);
+			col.attachedRigidbody.isKinematic = false;
+		}
+	}
+	
+	public override void onInteract(){
+		Debug.Log ("Grabbable triggered");
+		interactedOn = !interactedOn;
+	}
+	
 	override public bool isTriggered(){
-		return false;
+		return interactedOn;
 	}
 }
