7d7a244e661daba978de34b68061d87b0d2b4003
Cody <croge32@gmail.com>
2015-03-24 12:38:17 -0500

    updates to grabbable

--- DweebMcDermitt/Assets/Scripts/Interactable/Grabbable.cs
+++ DweebMcDermitt/Assets/Scripts/Interactable/Grabbable.cs
@@ -12,28 +12,54 @@ public class Grabbable : InteractionTarget {
 	private RaycastHit hit;
 	private Collider col;
 	private bool interactedOn = false;
+	private bool colliding = false;
+
+	private Camera camera;
+	private Vector3 center;
 	
 	void Start(){
+		camera = Camera.main;
+		center = transform.position;
 		player = GameObject.Find ("Player");
 		playerTransform = player.transform;
 		col = GetComponent<Collider> ();
 	}
 	
 	void Update () {
-		cameraPosition = Camera.main.transform.position;
+
+		center = camera.ScreenToWorldPoint(new Vector3(Screen.height/2, Screen.width/2, camera.nearClipPlane));
+		cameraPosition = camera.transform.position;
 		playerPosition = playerTransform.position;
 		if (interactedOn) {
-			transform.parent = Camera.main.transform;
+
+			transform.parent = camera.transform;
 			col.attachedRigidbody.useGravity = false;
 
 			// Stop out of control collision
-			col.attachedRigidbody.velocity = new Vector3(0,0,0);
+			if (colliding) {
+				print ("colliding");
+			} else {
+				col.attachedRigidbody.velocity = new Vector3(0,0,0);
+				col.attachedRigidbody.angularVelocity = new Vector3(0,0,0);
+				transform.rotation = Quaternion.LookRotation(-Camera.main.transform.forward, Camera.main.transform.up);
+				transform.position = camera.transform.position + camera.transform.forward * 1.5f;
+			}
 		} else {
 			transform.parent = null;
 			col.attachedRigidbody.useGravity = true;
 		}
 	}
-	
+
+	void OnCollisionEnter(Collision c) {
+		colliding = true;
+	}
+	void OnCollisionStay(Collision c) {
+		colliding = true;
+	}
+	void OnCollisionExit(Collision c) {
+		colliding = false;
+	}
+
 	public override void onInteract(){
 		Debug.Log ("Grabbable triggered");
 		interactedOn = !interactedOn;
