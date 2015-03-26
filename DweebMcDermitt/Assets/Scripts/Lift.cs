7696c7451b656c0d05368f22db9048fd3d3b9625
= <cleger35@gmail.com>
2015-03-26 16:37:41 -0500

    Added lift script

--- DweebMcDermitt/Assets/Scripts/Lift.cs
+++ DweebMcDermitt/Assets/Scripts/Lift.cs
@@ -1,11 +1,51 @@
-using UnityEngine;
-using System.Collections;
-/*
-public class Lift : TriggerTarget {
-
-	InteractionTarget trigger;
-
-	override public void onTrigger(InteractionTarget t) {
-		// go up....or perhaps...down?
+//when interacted upon, moves to the new position in specified time interval
+public class Lift : InteractionTarget{
+	[SerializeField] private Vector3 newPositionRelative;
+	[SerializeField] private float moveSpeed;
+	
+	private Vector3	newPosition;
+	private bool interactedOn = false;
+	private float startTime;
+	private float journeyDistance;
+	private Vector3 originalPosition;
+	private float journeyDistance2;
+	
+	[SerializeField] private AudioSource cubeAudio;
+	
+	//store the distance between our location and the new one
+	void Start(){
+		newPosition = transform.position + newPositionRelative;
+		journeyDistance = Vector3.Distance (transform.position, newPosition);
+		originalPosition = transform.position;
+		journeyDistance2 = Vector3.Distance (newPosition, originalPosition);
 	}
-}*/
+	
+	// If we're interacted upon, start moving the object to victory
+	void Update () {
+		if (interactedOn) {
+			transform.position = Vector3.Lerp (transform.position, 
+			                                   newPosition, 
+			                                   (Time.time - startTime) * moveSpeed / journeyDistance);
+		}
+		if (!interactedOn) {
+			transform.position = Vector3.Lerp (newPosition, originalPosition, 
+			                                   (Time.time - startTime) * moveSpeed / journeyDistance2);
+		}
+	}
+	
+	public override void onInteract(){
+		Debug.Log ("Lift triggered");
+		if (!interactedOn) {
+			startTime = Time.time;
+			cubeAudio.Play ();
+			
+		}
+		//prevent starting over
+		interactedOn = !interactedOn;
+		
+	}
+	
+	override public bool isTriggered(){
+		return interactedOn;
+	}
+}
