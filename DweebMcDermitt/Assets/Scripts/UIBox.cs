2f55c603784e0fef786bea3fd259c37b49b81dcc
Cody Rogers <croge32@gmail.com>
2015-03-26 17:28:39 -0500

    death text

--- DweebMcDermitt/Assets/Scripts/UIBox.cs
+++ DweebMcDermitt/Assets/Scripts/UIBox.cs
@@ -4,12 +4,20 @@ using System.Collections;
 public class UIBox : MonoBehaviour
 {
 	public bool dead = false;
-	public int depth = 5;
-	void OnGUI() {
+	public GameObject text;
+	Camera cam;
+
+	void Start() {
+		GameObject.Find ("rekt");
+	}
+
+	void Update() {
+		cam = Camera.main;
+		text.transform.parent = cam.transform;
+		text.transform.position = cam.transform.position + cam.transform.forward * 10.0f + -cam.transform.right * 7.0f;
 		if (dead) {
-			GUI.depth = depth;
 			print ("should be drawing");
-			GUI.Box (new Rect (0, 0, Screen.width, Screen.height), "U R DED");
+			text.GetComponent<MeshRenderer>().enabled = true;
 		}
 	}
 }
