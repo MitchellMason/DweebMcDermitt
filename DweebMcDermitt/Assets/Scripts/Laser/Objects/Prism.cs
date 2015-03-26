e56861d11b02ff39167c25b99c51a4a7a9b22946
Mitchell Mason <mmaso14@tigers.lsu.edu>
2015-03-25 23:32:48 -0500

    Imported new prism

--- DweebMcDermitt/Assets/Scripts/Laser/Objects/Prism.cs
+++ DweebMcDermitt/Assets/Scripts/Laser/Objects/Prism.cs
@@ -38,20 +38,27 @@ public class Prism : LaserTarget {
 		rRay.origin = start.Two;
 		lRay.direction = dir.Two;
 		
-//		lShooter.fireLaser(lRay);
-//		rShooter.fireLaser(rRay);
+		lShooter.fireLaser(lRay);
+		rShooter.fireLaser(rRay);
 	}
 	
 	override public void onLaserLeave(){}
 	
 	//Get the mesh positions of the new origins for the lasers.
 	private Vec3Tuple getNewOnMeshPosition(LaserHitInfo info){
-		return new Vec3Tuple (new Vector3(), new Vector3());
+		Vector3 hitPoint = info.hitPoint;
+		Vec3Tuple result = new Vec3Tuple(Vector3.zero, Vector3.zero);
+		
 	}
 	
 	//Get the directions for the lasers. 
 	private Vec3Tuple getNewLaserDirections(LaserHitInfo info){
 		return new Vec3Tuple (new Vector3(), new Vector3());
+	}
+	
+	//utility method that rotates a vector3 around some other point
+	private static Vector3 RotateAroundPoint(Vector3 point, Vector3 pivot, Quaternion angle){
+		return angle * ( point - pivot) + pivot;
 	}
 	
 	private class Vec3Tuple{
