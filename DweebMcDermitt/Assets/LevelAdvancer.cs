8f2824f177bca9a023930ac5883925c07db1e0b9
unknown <thefreemason11@gmail.com>
2015-03-26 15:22:14 -0700

    Added sound and level transitions

---
+++ DweebMcDermitt/Assets/LevelAdvancer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelAdvancer : MonoBehaviour {
+
+	[SerializeField] int levelNumber;
+	void OnTriggerEnter(Collider col){
+		if (col.tag.Equals ("Player")) {
+			Debug.Log("Loading Level " + levelNumber);
+			Application.LoadLevel(levelNumber);
+		}
+	}
+}
