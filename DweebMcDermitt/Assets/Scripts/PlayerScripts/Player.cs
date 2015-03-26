debe33916d6d764cf558a81aec6c82dc5d2de8e4
Cody Rogers <croge32@gmail.com>
2015-03-25 21:56:19 -0500

    trying to figure out GUI shit

--- DweebMcDermitt/Assets/Scripts/PlayerScripts/Player.cs
+++ DweebMcDermitt/Assets/Scripts/PlayerScripts/Player.cs
@@ -2,7 +2,31 @@
 using System.Collections;
 
 public class Player : LaserTarget {
-	override public void onLaserShot(LaserHitInfo laserHitInfo){}
+
+	float HP = 3;
+	bool dead = false;
+	GameObject ui;
+	UIBox uibox;
+
+	void Start() {
+		ui = GameObject.Find ("GUI");
+		uibox = ui.GetComponent<UIBox>();
+		if (uibox) {
+			print ("got it");
+		}
+	}
+
+	override public void onLaserShot(LaserHitInfo laserHitInfo){
+		HP -= 1;
+		print (HP);
+		if (HP <= 0) {
+			dead = true;
+			uibox.dead = true;
+			print ("dead");
+			GetComponent<CharacterController>().enabled = false;
+		}
+	}
+
 	override public void onLaserStay(LaserHitInfo laserHitInfo){}
 	override public void onLaserLeave(){}
 	
