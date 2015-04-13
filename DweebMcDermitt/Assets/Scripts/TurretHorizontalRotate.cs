3bbdd3f7970ddc61a106525da413458cd14ee9c0
James Smith <jsmi359@lsu.edu>
2015-04-12 21:33:26 -0500

    Adding Level 3 Grayblock and first edition turret

---
+++ DweebMcDermitt/Assets/Scripts/TurretHorizontalRotate.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretHorizontalRotate : MonoBehaviour {
+
+	// fast rotation
+	public float rotSpeed = 360f;
+
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+		// distance between target and the actual rotating object
+		Vector3 D = target.position - transform.position;  
+		
+		
+		// calculate the Quaternion for the rotation
+		Quaternion rot = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(D), rotSpeed * Time.deltaTime);
+		
+		//Apply the rotation 
+		transform.rotation = rot; 
+
+		// put 0 on the axys you do not want for the rotation object to rotate
+		transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+	
+	}
+}
