7ee0710b6a7ff388eb939e4a9658eb2a69e7c946
akemp <akemp1@lsu.edu>
2015-04-18 17:22:53 -0500

    Commit new waveguide

--- DweebMcDermitt/Assets/Scripts/Laser/Objects/MagnifyingGlass.cs
+++ DweebMcDermitt/Assets/Scripts/Laser/Objects/MagnifyingGlass.cs
@@ -2,11 +2,60 @@
 using System.Collections;
 
 public class MagnifyingGlass : LaserTarget {
-	override public void onLaserShot(LaserHitInfo laserHitInfo){}
-	override public void onLaserStay(LaserHitInfo laserHitInfo){}
-	override public void onLaserLeave(){}
 	
-	//TODO
+	Vector3 newDir;
+	
+	public LineRenderer lineRenderer;
+	public LaserShooter shooter;
+	public void startUp()
+	{
+		if (gameObject.GetComponents<LineRenderer> ().Length <= 0)
+			lineRenderer = gameObject.AddComponent<LineRenderer> ();
+		else {
+			lineRenderer = GetComponent<LineRenderer> ();
+		}
+		shooter = new LaserShooter (lineRenderer);
+	}
+	void Awake(){
+		startUp ();
+	}
+	
+	void Start(){
+		startUp ();
+	}
+	override public void onLaserShot(LaserHitInfo laserHitInfo){
+		lineRenderer.enabled = true;
+		shoot (laserHitInfo);
+	}
+	
+	override public void onLaserStay(LaserHitInfo laserHitInfo){
+		lineRenderer.enabled = true;
+		shoot (laserHitInfo);
+	}
+	
+	override public void onLaserLeave(){
+		lineRenderer.enabled = false;
+		shooter.endFire ();
+	}
+	
+	void shoot(LaserHitInfo laserHitInfo){
+		//Debug.DrawRay (laserHitInfo.hitPoint, laserHitInfo.hitSurfaceNormal, Color.green);
+		Vector3 inDir = -(laserHitInfo.EmitterPosition-laserHitInfo.hitPoint).normalized;
+		//Debug.DrawRay (laserHitInfo.EmitterPosition, inDir * laserHitInfo.remainingDistance, Color.red);
+		//newDir = Vector3.Reflect(inDir, laserHitInfo.hitSurfaceNormal).normalized;
+		//Debug.Log ("in: " + laserHitInfo.EmitterPosition + " hit: " + laserHitInfo.hitPoint + " normal: " + laserHitInfo.hitSurfaceNormal + " outDir: " + newDir + " end: " + laserEndPoint);
+		float angle = Vector3.Dot (inDir, transform.forward);
+
+		{
+			float ratio = (1.0f-Mathf.Abs (angle));
+
+			Ray ray = new Ray (laserHitInfo.hitPoint, Mathf.Sign(angle)*transform.forward*ratio + inDir * (1.0f-ratio));
+			//Debug.DrawRay (ray.origin, ray.direction, Color.blue);
+			//shooter.fireLaser(ray, laserHitInfo.remainingDistance);
+			shooter.fireLaser (ray, LaserUtils.LASER_DISTANCE);
+		}
+	}
+	
 	override public bool isTriggered(){
 		return false;
 	}
