e20ffa3deb52bd05120be274f046c4c6b5688010
James Smith <jsmi359@lsu.edu>
2015-04-23 17:42:38 -0500

    Level 3 has first good turret

--- DweebMcDermitt/Assets/Scripts/TurretVerticalRotate.cs
+++ DweebMcDermitt/Assets/Scripts/TurretVerticalRotate.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TurretHorizontalRotate : TriggerTarget {
+public class TurretVerticalRotate : MonoBehaviour {
 	
 	// fast rotation
 	[SerializeField] float rotSpeed;
@@ -11,49 +11,39 @@ public class TurretHorizontalRotate : TriggerTarget {
 	[SerializeField] Transform target;
 	[SerializeField] Transform laserStartPoint;
 	[SerializeField] Transform laserEndPoint;
-	[SerializeField] float range;
-	RaycastHit hit;
-	bool dead;
 	
 	// Use this for initialization
 	void Start () {
 		target = GameObject.FindGameObjectWithTag ("Player").transform;
 		gun = new LaserShooter (lineRenderer);
-		dead = false;
 		//laserStartPoint = 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (dead) {
-			return;
-		}
-		Physics.Raycast(transform.position, target.position - transform.position, out hit, range);
-		if (!hit.collider.gameObject.name.Equals("Player") && !hit.collider.gameObject.name.Equals("PlayerToggle")) {
-			return;
-		}
+		float yRotation = transform.eulerAngles.y;
+		
 		// distance between target and the actual rotating object
 		Vector3 D = target.position - transform.position;  
-
+		
+		
+		
 		// calculate the Quaternion for the rotation
 		Quaternion rot = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(D), rotSpeed * Time.deltaTime);
-
+		
+		
+		
 		//Apply the rotation 
 		transform.rotation = rot; 
 		
 		// put 0 on the axys you do not want for the rotation object to rotate
-		transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+		transform.eulerAngles = new Vector3(-13, yRotation, 0);
 		
-		gun.fireLaser (new Ray (laserStartPoint.position, laserEndPoint.position - laserStartPoint.position), range);
+		Vector3 LaserDirection = transform.eulerAngles;
+		LaserDirection.y += 1;
+		
+		//gun.fireLaser (new Ray (laserStartPoint.position, laserEndPoint.position - laserStartPoint.position), 50f);
 		
 		////gun.fireLaser (new Ray (new Vector3 (transform.position.x + 0f, transform.position.y - 0.1f, transform.position.z + 0f), LaserDirection), 20f);
-	}
-
-	public void die() {
-		dead = true;
-	}
-
-	public override void onTrigger(MonoBehaviour trigger) {
-		die ();
 	}
 }
