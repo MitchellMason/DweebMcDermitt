e28e2015962382dfb3e93d2bbea04d00d75a4ab2
Cody Rogers <croge32@gmail.com>
2015-04-30 16:20:47 -0500

    changed turret horizontal detection and added splash screen

--- DweebMcDermitt/Assets/Scripts/TurretHorizontalRotate.cs
+++ DweebMcDermitt/Assets/Scripts/TurretHorizontalRotate.cs
@@ -18,39 +18,46 @@ public class TurretHorizontalRotate : TriggerTarget {
 	RaycastHit hit;
 	bool dead;
 	bool soundHasPlayed = false;
+	public bool detected;
 	// Use this for initialization
 	void Start () {
 		target = GameObject.FindGameObjectWithTag ("Player").transform;
 		gun = new LaserShooter (lineRenderer);
 		dead = false;
+		detected = false;
 		//laserStartPoint = 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (dead) {
-			gun.endFire();
+			detected = false;
+			//gun.endFire();
 			return;
 		}
 		Physics.Raycast(transform.position, target.position - transform.position, out hit, range);
-		if (!hit.collider.gameObject.name.Equals("Player") && !hit.collider.gameObject.name.Equals("PlayerToggle")) {
-			gun.endFire();
+		if (!hit.collider.gameObject.name.Equals ("Player") && !hit.collider.gameObject.name.Equals ("PlayerToggle")) {
+			detected = false;
+			//gun.endFire();
 			return;
+		} else {
+			detected = true;
 		}
-		// distance between target and the actual rotating object
-		Vector3 D = target.position - transform.position;  
+		if (detected) {
+			// distance between target and the actual rotating object
+			Vector3 D = target.position - transform.position;  
 
-		// calculate the Quaternion for the rotation
-		Quaternion rot = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(D), rotSpeed * Time.deltaTime);
+			// calculate the Quaternion for the rotation
+			Quaternion rot = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (D), rotSpeed * Time.deltaTime);
 
-		//Apply the rotation 
-		transform.rotation = rot; 
+			//Apply the rotation 
+			transform.rotation = rot; 
 		
-		// put 0 on the axys you do not want for the rotation object to rotate
-		transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+			// put 0 on the axys you do not want for the rotation object to rotate
+			transform.eulerAngles = new Vector3 (0, transform.eulerAngles.y, 0);
 		
-		gun.fireLaser (new Ray (laserStartPoint.position, laserEndPoint.position - laserStartPoint.position), range);
-		
+			gun.fireLaser (new Ray (laserStartPoint.position, laserEndPoint.position - laserStartPoint.position), range);
+		}
 		////gun.fireLaser (new Ray (new Vector3 (transform.position.x + 0f, transform.position.y - 0.1f, transform.position.z + 0f), LaserDirection), 20f);
 	}
 
