7bf78cc1622539603d622ba93d027a538da4e231
= <cleger35@gmail.com>
2015-04-30 17:18:55 -0500

    ending dialogue plays at top of lift

---
+++ DweebMcDermitt/Assets/Scripts/LiftWithDialogue.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//when interacted upon, moves to the new position in specified time interval
+public class LiftWithDialogue : TriggerTarget{
+	[SerializeField] private Vector3 newPositionRelative;
+	[SerializeField] private float moveSpeed;
+	
+	private Vector3	newPosition;
+	private bool isTriggered = false;
+	private float startTime;
+	private float journeyDistance;
+	
+	//public AudioSource door;
+	[SerializeField] private AudioSource liftAudio;
+	[SerializeField] private AudioSource endingDialogue;
+	
+	//store the distance between our location and the new one
+	void Start(){
+		newPosition = transform.position + newPositionRelative;
+		journeyDistance = Vector3.Distance (transform.position, newPosition);
+	}
+	
+	// If we're interacted upon, start moving the object to victory
+	void Update () {
+		if (isTriggered) {
+			transform.position = Vector3.Lerp(transform.position, 
+			                                  newPosition, 
+			                                  (Time.time - startTime) * moveSpeed / journeyDistance);
+		}
+	}
+	
+	public override void onTrigger(MonoBehaviour trigger){
+		Debug.Log ("Bookshelf triggered");
+		if (!isTriggered) {
+			startTime = Time.time;
+			liftAudio.Play ();
+			if(liftAudio != null) endingDialogue.PlayDelayed(7f);
+		}
+		//prevent starting over
+		isTriggered = true;
+	}
+	
+	//	override public bool isTriggered(){
+	//		return interactedOn;
+	//	}
+}
